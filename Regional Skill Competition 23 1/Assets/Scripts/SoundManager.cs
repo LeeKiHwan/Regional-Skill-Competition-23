e9f29b23b0f8a875fa47eb7f9a19507e25dbe757
LeeKiHwan <s22049@gsm.hs.kr>
2023-03-13 17:55:05 +0900

    사운드매니저 및 사운드 이펙트 추가

---
+++ Regional Skill Competition 23 1/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (instance == null) return null;
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Debug.Log("SoundManager Instance Error");
+            Destroy(gameObject);
+        }
+    }
+
+    public void SFXPlay(string sfxName, AudioClip clip)
+    {
+        GameObject go = new GameObject(sfxName + "Sound");
+        AudioSource audioSource = go.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.Play();
+
+        Destroy(go, clip.length);
+    }
+}
